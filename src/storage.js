68939c57fd75ab72695888ad4d346cbdf73ea9f0
Miiio <machenzie@hotmail.com>
2026-09-24 15:38:06 +1200

    Add local IndexedDB storage

---
+++ src/storage.js
@@ -0,0 +1,49 @@
+const DB_NAME = 'keepmyreceipt-db'
+const DB_VERSION = 1
+const STORE = 'purchases'
+
+function openDb() {
+  return new Promise((resolve, reject) => {
+    const request = indexedDB.open(DB_NAME, DB_VERSION)
+
+    request.onupgradeneeded = () => {
+      const db = request.result
+      if (!db.objectStoreNames.contains(STORE)) {
+        db.createObjectStore(STORE, { keyPath: 'id' })
+      }
+    }
+
+    request.onsuccess = () => resolve(request.result)
+    request.onerror = () => reject(request.error)
+  })
+}
+
+export async function getPurchases() {
+  const db = await openDb()
+  return new Promise((resolve, reject) => {
+    const tx = db.transaction(STORE, 'readonly')
+    const req = tx.objectStore(STORE).getAll()
+    req.onsuccess = () => resolve(req.result || [])
+    req.onerror = () => reject(req.error)
+  })
+}
+
+export async function savePurchase(purchase) {
+  const db = await openDb()
+  return new Promise((resolve, reject) => {
+    const tx = db.transaction(STORE, 'readwrite')
+    tx.objectStore(STORE).put(purchase)
+    tx.oncomplete = () => resolve(purchase)
+    tx.onerror = () => reject(tx.error)
+  })
+}
+
+export async function deletePurchase(id) {
+  const db = await openDb()
+  return new Promise((resolve, reject) => {
+    const tx = db.transaction(STORE, 'readwrite')
+    tx.objectStore(STORE).delete(id)
+    tx.oncomplete = () => resolve()
+    tx.onerror = () => reject(tx.error)
+  })
+}
